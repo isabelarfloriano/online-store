878b56736abed5817b7b1179946af2dec2b04cf1
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-04-26 17:44:33 -0300

    create: Home page and route in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
 // import * as api from './services/api';
 
-class App extends React.Component {
+class App extends Component {
   /* constructor() {
     super();
     this.state = {
@@ -23,20 +24,11 @@ class App extends React.Component {
     api.getProductsFromCategoryAndQuery(categoryId, query)
       .then((categories) => { console.log(categories); }); */
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>Edit src/App.js and save to reload.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
